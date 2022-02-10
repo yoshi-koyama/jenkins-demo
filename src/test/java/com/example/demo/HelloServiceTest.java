ea39836ebf43d0f491b4b8545c7df1fcfb950608
ThiriYadanar <tryatanar@gmail.com>
2022-02-10 17:13:44 +0630

    Update HelloServiceTest.java

--- src/test/java/com/example/demo/HelloServiceTest.java
+++ src/test/java/com/example/demo/HelloServiceTest.java
@@ -16,6 +16,6 @@ class HelloServiceTest {
 
     @Test
     void helloが取得できるか() {
-        assertThat(helloService.hello()).isEqualTo("hello");
+        assertThat(helloService.hello()).isEqualTo("hllo");
     }
 }
